4541873f8dfc2a146178d77fe19c14a52d5f56f3
Sijadkhan1678 <sijadkhan1678@gmail.com>
2022-09-05 22:18:14 +0500

    initial commit for project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+
+const  App = () => {
+
+  
+  return (
+    <div className="App">
+      
+    wellcome in shoe Store
+    </div>
+  );
+}
+
+export default App;
