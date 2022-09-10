f90bb981a6a2c5035bb6f9235246029b345e81d5
Sijadkhan1678 <sijadkhan1678@gmail.com>
2022-09-10 17:02:47 +0500

    Added routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import './App.css';
+import React,{Fragment} from 'react'
+import Container from '@mui/material/Container'
+import Header from './components/layout/Header'
+import Home from './components/pages/Home'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 const  App = () => {
 
   
   return (
-    <div className="App">
-      
-    wellcome in shoe Store
-    </div>
+   <Router>
+     <Fragment>
+      <Header />
+    
+  <Container maxWidth="md" >
+    <Routes>
+      <Route path="/" element={<Home />}>
+        
+      </Route>
+    </Routes>
+   </Container>
+  
+    </Fragment>
+   </Router>
+    
   );
 }
 
