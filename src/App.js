462c6d189c6375880214601f6748c798abffd6ff
sijadkhan1678 <sijadkhan1678@gmail.com>
2023-11-07 19:56:02 +0500

    Updated App and restyled header components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React,{Fragment} from 'react'
-import Container from '@mui/material/Container'
+import React, { Fragment } from 'react'
+import Box from '@mui/material/Box'
 import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 import Home from './components/pages/Home';
 import Brand from './components/pages/Brand'
 import About from './components/pages/About'
-import Cart  from './components/pages/Cart'
+import Cart from './components/pages/Cart'
 import NotFound from './components/pages/NotFound'
 import AppState from './context/AppState'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-const  App = () => {
+const App = () => {
 
-  
+
   return (
-   <AppState>
-   <Router>
-     <Fragment>
-      <Header />
-    
-  <Container maxWidth="md" >
-    <Routes>
-    
-      <Route path="/"    element={<Home />} />
-     <Route path="brands" element={<Brand />} />
-      <Route path="about" element={<About />} />
-      <Route path="cart"  element={<Cart />} />
-      <Route path="*"     element={<NotFound />} />
-    </Routes>
-   </Container>
-  
-    </Fragment>
-   </Router>
+    <AppState>
+      <Router>
+        <Fragment>
+          <Header />
+          <Box>
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="brands" element={<Brand />} />
+              <Route path="about" element={<About />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+
+          </Box>
+           <Footer />
+        </Fragment>
+      </Router>
     </AppState>
   );
 }
 
 export default App;
+
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,55 +1,54 @@
-import React,{useContext} from 'react';
-import {Box,Stack,Badge,IconButton} from '@mui/material'
+import React, { useContext } from 'react';
+import { Box, Stack, Badge, IconButton } from '@mui/material'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import logo from './logo.jpg'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import context from '../../context/AppContext'
 
 const Header = () => {
-  
-  const {cart} = useContext(context)
-   return (
-        <Box p={4} px={8} >
-           <Stack direction='row' justifyContent='space-between' 
-           alignItems='center'>
-             <Box width='10px'>
-             <img width='40px' src={logo} alt='logo'/>
-             </Box>
-             <Stack direction='row' justifyContent='space-between'
-             width='30%'>
-             <NavLink to='/' style={style}>Home</NavLink>
-             <NavLink to='/brands' style={style}>Brands</NavLink>
-             <NavLink to='/about' style={style}>About</NavLink>
-             </Stack>
-             <Box width='13%' > 
 
-        <IconButton
-              aria-label='cart' 
-              size='large'
-              p={5}  >
-          <NavLink style={{color:'orange'}} to='/cart'>
-           <Badge badgeContent={cart.length}
-                  color='primary'>
-                  
-            <ShoppingCartIcon  />
-      
-           </Badge>
-           </NavLink>
-        </IconButton>
-     
-             </Box>
-            </Stack>
-        </Box>
-      
-        )
-  
+     const { cart } = useContext(context)
+     return (
+          <Box p={3} px={8}>
+               <Stack direction='row' justifyContent='space-between'
+                    alignItems='center'>
+                    <Box width='50px' marginTop="6px">
+                         <img width='50px' src={logo} alt='logo' />
+                    </Box>
+                    <Stack direction='row' justifyContent='space-between'
+                         width='30%' ml="24rem">
+                         <NavLink to='/' style={style}>Home</NavLink>
+                         <NavLink to='/brands' style={style}>Brands</NavLink>
+                         <NavLink to='/about' style={style}>About</NavLink>
+                    </Stack>
+                    <Box width='13%' >
+
+                         <IconButton
+                              aria-label='cart'
+                              size='large'
+                              p={5}  >
+                              <NavLink style={{ color: '#e09d51' }} to='/cart'>
+                                   <Badge badgeContent={cart.length} color='primary'>
+                                        <ShoppingCartIcon />
+
+                                   </Badge>
+                              </NavLink>
+                         </IconButton>
+
+                    </Box>
+               </Stack>
+          </Box>
+
+     )
+
 }
-const style = ({isActive}) => {
- return { fontWieght: '800px',
-  fontSize: '1.3rem',
-  textDecoration: 'none',
-  color: 'orange',
-  borderBottom: isActive ? '3px solid orange' :''
- }
+const style = ({ isActive }) => {
+     return {
+          fontWieght: '800px',
+          fontSize: '1.3rem',
+          textDecoration: 'none',
+          color: '#97670f',
+          borderBottom: isActive ? '3px solid #b78f46' : ''
+     }
 }
 export default Header;
