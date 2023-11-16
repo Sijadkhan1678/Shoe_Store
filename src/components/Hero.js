72757bcb8e83e115b4879635e398403805ee7d62
sijadkhan1678 <sijadkhan1678@gmail.com>
2023-11-16 12:35:04 +0500

    Added Responsiveness to Hero compoponent

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,40 +4,58 @@ import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+import { useMediaQuery } from '@mui/material';
 import hero from './hero3.png'
 
 const Hero = () => {
+
+  const xs = 600;
+  const sm = 900;
+  const md = 1200;
+  const lg = 1536;
+
+  const isMobile = useMediaQuery(`(max-width: ${xs}px)`);
+  const isSmall = useMediaQuery(`(max-width: ${sm}px)`);
+  const isLarge = useMediaQuery(`(min-width: ${md}px)`);
+
+  const imageStyle = {
+    width: '100%',
+    maxWidth: isLarge ? 700 : '100%',
+    height: isLarge ? 500 : (isSmall ? (isMobile ? 261 : 400) : 450)
+  };
+
   return (
     <Container>
-      <Box width='100%' px="2.5rem" pt="0rem" boxSizing="border-box" pb="3rem">
+      <Box width='100%' px={{ lg: '2rem', md: '2.5rem', sm: '2.5rem', xs: '0rem' }} pt="0rem" boxSizing="border-box" pb="3rem">
         <Stack direction='row' justifyContent='space-between' >
-          <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '50%',
+          <Box sx={{ width: '50%', marginTop: { md: '1.5rem', sm: '1.5rem', xs: '1.7rem' } }}>
 
-          }}>
-            <Typography variant='h1' color='orange' fontSize="7rem" fontWeight="bold" ml="-4px" p="12px 0 10px 0">  New Nike series</Typography>
-            <Typography variant='p' fontSize="14px" color='#684815'>
+            <Typography variant='h1' color='orange' fontSize={{ lg: "7rem", md: '5.64rem', sm: '3.82rem', xs: '2.43rem' }} fontWeight="bold" ml="-4px" p={{ sm: "32px 0 10px 0", md: '12px 0 10px' }}>  New Nike series</Typography>
+            <Typography variant='p' fontSize={{ lg: "14px", md: '14px', sm: '14', xs: '10px' }} color='#684815'>
               purchase awesome shoes with high quality
               original branded shoes with fastest delivery on your door step
             </Typography>
-            <Button variant='contained' size='small'
-              style={{
+
+            <Button variant='contained' size="small"
+              sx={{
                 backgroundColor: 'orange',
                 padding: '8px 15px',
-                marginTop: '1.8rem',
-                marginLeft: '-25rem',
+                mt: { lg: '1.8rem', md: '1.8rem', sm: '1.8rem', xs: '1rem' },
+                fontSize: { lg: '0.8125rem', md: '0.8125rem', sm: "0.8125rem", xs: '0.4125rem' },
+                '&:hover': { bgcolor: '#e09d51' },
+                display: 'block',
                 borderRadius: '3px'
-              }}>
+              }}
+            >
               explore now
             </Button>
+
           </Box>
 
-          <Box width='50%' >
-            <img src={hero} alt='hero' width='100%' height='500' />
+          <Box width='50%'>
+
+            <img src={hero} alt='hero' style={imageStyle} />
+
           </Box>
         </Stack>
       </Box>
