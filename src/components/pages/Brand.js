c07005216ab931e13e17823388149e1e72f71d6c
sijadkhan1678 <sijadkhan1678@gmail.com>
2023-11-16 12:48:05 +0500

    Implemented brand selection functionality in Brand page

--- src/components/pages/Brand.js
+++ src/components/pages/Brand.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import context from '../../context/AppContext'
 import BrandItem from '../BrandItem'
-import Products from '../products/Products'
-import {Box,Typography} from '@mui/material'
+import ProductItem from '../products/ProductItem'
+import { Container, Box, Grid, Typography } from '@mui/material'
 
 const Brands = () => {
-  
-     return (
-      <Box mt={6}> 
-        <Typography variant='h4'> 
+
+  const { products, brandProducts, selectedBrand } = useContext(context);
+
+
+  const selectedProducts = selectedBrand === 'All' || selectedBrand === 'Home' ? products : brandProducts
+
+  return (
+    <Box mt={6}>
+      <Container>
+        <Typography variant='h5' m="-14px" ml="4rem" fontSize="1.8rem" fontWeight="500" >
           Brands
         </Typography>
         <BrandItem />
-        <Products /> 
-     
-     </Box>
-       )
+        <Grid container justifyContent='space-between' spacing={6}>
+          {selectedProducts.map(product => (<ProductItem key={product.id} product={product} />))}
+        </Grid>
+      </Container>
+    </Box>
+  )
 }
 export default Brands;
