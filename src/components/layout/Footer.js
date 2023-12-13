cb63d113aff4b075c8d5693b694474fbbec88d01
sijadkhan1678 <sijadkhan1678@gmail.com>
2023-12-13 09:55:52 +0500

    Added Reponsiveness to Footer component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,14 +3,13 @@
  import { Link, NavLink } from 'react-router-dom'
  import logo from './logo.jpg'
 
- //fontWeight={400}
  
  const Footer = () => {
    return (
      <Box mt="3rem" pt="3rem" px="0.9rem" bgcolor="#EDF1FF">
-         <Stack pt="3rem" px="3rem"  direction="row" justifyContent="space-between">
-            <Box pl="15px" pr="3rem" mb="3rem">
-            <Box width='50px' marginBottom="1.5rem">
+         <Stack pt="3rem" px={{lg:"3rem",md:'1.5rem'}}  direction={{lg:'row',md:"row",sm:'column'}} justifyContent="space-between">
+            <Box pl={{lg:"15px",sm:'0px',xs:'15px'}} pr="3rem" ml={{lg:'0rem',sm:'0rem'}} mb="3rem">
+            <Box width='50px' mb="1.5rem">
              <img width='50px' src={logo} alt='logo'/>
              </Box>
              
@@ -21,7 +20,8 @@
               <Typography variant='body1'>+012 345 67890</Typography>
               </Box>
             </Box>
-            <Box pl="15px" pr="15px" mb="3rem" ml="-80px">
+            <Stack direction={{lg:'row',sm:'row',sx:'column'}} flex='0 0 66%' justifyContent={{lg:'space-between',md:'space-between',sm:'space-around'}}>
+            <Box pl={{lg:'15px',md:"15px",sm:'70px',xs:'15px'}} pr="15px" mb="3rem" ml={{lg:"-80px",md:'-80px',sm:'-80px',xs:'0px'}}>
                 <Typography variant='h5' fontSize="1.25rem" color="#2c2c2c" fontWeight={700} marginBottom="1.5rem">
                   Quick Shop
                 </Typography>
@@ -52,24 +52,25 @@
 
 
             </Box>
-            <Box pl="30px" mb="3rem">
+            <Box pl={{lg:'30px',md:'30px',sm:"30px",xs:'15px'}} pr={{xs:"1rem"}} mb="3rem">
                 <Typography variant="h5" fontSize="1.25rem" fontWeight={700} color="#2c2c2c" marginBottom="1.5rem">
                     Newsletter
                 </Typography>
                 <Box mb="1rem">
-                <TextField label="Your Name"  bgcolor="whitesmoke" />
+                <TextField fullWidth={{sm:"true"}} label="Your Name"  bgcolor="whitesmoke" />
                 </Box>
                 <Box mb="1rem">
-                <TextField label="Your Email" bgcolor="whitesmoke" />
+                <TextField fullWidth={{sm:"true"}} label="Your Email" bgcolor="whitesmoke" />
                 </Box>
                 
                 <Button fullWidth variant="contained" sx={{color:"#000000",bgcolor:"#ffa71f",'&:hover':{color:'whitesmoke'}}}  size="large">Subscribe Now</Button>
         
                 
             </Box>
+            </Stack>
           
          </Stack>
-         <Stack direction="row" alignItems='center' mx="3rem" py="1.3rem">
+         <Stack direction="row" flexWrap='wrap' alignItems='center' mx={{lg:'3rem',md:'3rem',sm:'orem'}} py="1.3rem">
          ©  <Typography variant='body1' fontWeight={600} ml={1}> Your Site Name.</Typography>
            <Typography variant="body1" ml={1}>All Rights Reserved. Designed by</Typography>
            <Typography variant='body1' fontWeight={600} ml={1}>HTML Codex</Typography>
