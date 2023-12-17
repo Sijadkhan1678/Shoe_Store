567620360f6f367d30add9145be603124a220d47
sijadkhan1678 <sijadkhan1678@gmail.com>
2023-12-17 15:57:00 +0500

    check product exist in cart logic tranfered to AppReducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,43 +1,62 @@
-import {GET_BRAND_PRODUCTS,ADD_TO_CART,REMOVE_FROM_CART,INCREASE_QUANTITY,DECREASE_QUANTITY, CHANGE_BRAND} from './Types';
+import { GET_BRAND_PRODUCTS, ADD_TO_CART, REMOVE_FROM_CART, INCREASE_QUANTITY, DECREASE_QUANTITY, CHANGE_BRAND } from './Types';
 
-const AppReducer = (state,action) => {
+const AppReducer = (state, action) => {
+  
   switch (action.type) {
-    case GET_BRAND_PRODUCTS:
-      return {
- ...state,brandProducts: state.products.filter(product => product.brand === action.payload),
-    selectedBrand:action.payload
-      }
-  
-      case ADD_TO_CART:
-      //  console.log(...action.payload)
-        return {
-        ...state,cart: [...state.cart,action.payload]
-        }
-      case REMOVE_FROM_CART:
-        return {
-        ...state, cart: state.cart.filter(item=> item.id !== action.payload)
-        }
-      case INCREASE_QUANTITY:
-        return {
-          ...state,cart: state.cart.filter(item=> item.id !== action.payload ? item: item.quantity++ )
-          
-        }
-        case DECREASE_QUANTITY:
 
-          if (action.payload.quantity > 1) 
-    
-            return {
-              ...state, cart: state.cart.map(product => product.id !== action.payload.id ? product : { ...product, quantity: action.payload.quantity - 1 })
-            }
-            
-      case CHANGE_BRAND:
+    case GET_BRAND_PRODUCTS:
+
+      return {
+        ...state, brandProducts: state.products.filter(product => product.brand === action.payload),
+        selectedBrand: action.payload
+      }
+
+    case ADD_TO_CART:
+
+      const productIds = state.cart.map(product => product.id);
+      const isIdExsist = productIds.find(id => id === action.payload.id);
+      
+      if (!isIdExsist) { 
+        
+        return { ...state, cart: [...state.cart, { ...action.payload }] }
+
+      }
+
+      else {
+
         return {
-          ...state, selectedBrand: action.payload
+
+          ...state, cart: state.cart.map(product => product.id !== isIdExsist ? product : { ...product, quantity: product.quantity + 1 })
         }
+
+      }
+
+    case REMOVE_FROM_CART:
+      return { ...state, cart: state.cart.filter(item => item.id !== action.payload) }
+      
+    case INCREASE_QUANTITY:
+
+      return {
+        ...state, cart: state.cart.map(product => product.id !== action.payload ? product : { ...product, quantity: product.quantity + 1 })
+
+      }
+    case DECREASE_QUANTITY:
+
+      if (action.payload.quantity > 1)
+
+        return {
+          ...state, cart: state.cart.map(product => product.id !== action.payload.id ? product : { ...product, quantity: action.payload.quantity - 1 })
+        }
+
+    case CHANGE_BRAND:
+      return {
+        ...state, selectedBrand: action.payload
+      }
+
     default:
-     return  state
+      return state
   }
-  
+
 }
 
 export default AppReducer;
--- src/context/AppState.js
+++ src/context/AppState.js
@@ -1,73 +1,68 @@
-import React, {useReducer} from 'react';
+import React, { useReducer } from 'react';
 import AppReducer from './AppReducer';
 import context from './AppContext';
 import shoesList from './Data'
-import {GET_BRAND_PRODUCTS,ADD_TO_CART,REMOVE_FROM_CART,INCREASE_QUANTITY,DECREASE_QUANTITY,CHANGE_BRAND} from './Types';
+import { GET_BRAND_PRODUCTS, ADD_TO_CART, REMOVE_FROM_CART, INCREASE_QUANTITY, DECREASE_QUANTITY, CHANGE_BRAND } from './Types';
 
-const AppState = ({children}) => {
-  
+const AppState = ({ children }) => {
+
   const initalState = {
-       products: shoesList ,
-       brandProducts:[],
-       cart:[],
-       selectedBrand: 'Home'
+    products: shoesList,
+    brandProducts: [],
+    cart: [],
+    selectedBrand: 'Home'
   }
-  
-  const [state,dispatch] = useReducer(AppReducer,initalState);
-  
- 
+
+
+  const [state, dispatch] = useReducer(AppReducer, initalState);
+
+
   //function  get Brand products 
   const getBrandProducts = (brand) => {
     console.log(brand)
-      dispatch({
-        type: GET_BRAND_PRODUCTS,
-        payload: brand
-      })
-  }
-    
-  // function add to cart product
-  const addToCart = (product) => {
-    
-const item=state.cart.map((item,index)=>
-item.id === product.id ? product.id : null
-);
-  
-     if(item.includes(product.id))
-     {
-       increaseQuantity(product.id)
-     }
-  else{
     dispatch({
-      type: ADD_TO_CART,
-      payload: {...product,quantity:1}
+      type: GET_BRAND_PRODUCTS,
+      payload: brand
     })
   }
-  }
-  // function remove from cart product
-    const removeFromCart = id => {
-        
-        dispatch({
-          type: REMOVE_FROM_CART,
-          payload: id
-        })
-    }
-  
-  // function  increase Item qunatity in cart 
-  const increaseQuantity =  id => {
+
+  // function add to cart product
+  const addToCart = (product) => {
+
     dispatch({
-       type: INCREASE_QUANTITY,
-       payload: id
-      })
-  }
-  // function  decrease productItem qunatity in cart 
-  const decreaseQuantity = ({id,quantity}) => {
-    
-    dispatch({
-      type: DECREASE_QUANTITY,
-      payload: {id,quantity}
+      type: ADD_TO_CART,
+      payload: { ...product, quantity: 1 }
     })
 
   }
+
+  // function remove from cart product
+  const removeFromCart = id => {
+
+    dispatch({
+      type: REMOVE_FROM_CART,
+      payload: id
+    })
+  }
+
+  // function to increase Item qunatity in cart 
+  const increaseQuantity = id => {
+
+    dispatch({
+      type: INCREASE_QUANTITY,
+      payload: id
+    })
+  }
+  // function to decrease productItem qunatity in cart 
+  const decreaseQuantity = ({ id, quantity }) => {
+
+    dispatch({
+      type: DECREASE_QUANTITY,
+      payload: { id, quantity }
+    })
+
+  }
+
   // function to change brand name 
   const changeBrand = (brand) => {
     dispatch({
@@ -75,24 +70,24 @@ item.id === product.id ? product.id : null
       payload: brand
     })
   }
-  
+
   return (
-       <context.Provider value={{
-          products: state.products,
-          cart: state.cart,
-          selectedBrand: state.selectedBrand,
-          brandProducts: state.brandProducts,
-          getBrandProducts,
-          addToCart,
-          removeFromCart,
-          increaseQuantity,
-          decreaseQuantity,
-          changeBrand
-       }}>
-       {children}
-       </context.Provider>
-    )
-  
+    <context.Provider value={{
+      products: state.products,
+      cart: state.cart,
+      selectedBrand: state.selectedBrand,
+      brandProducts: state.brandProducts,
+      getBrandProducts,
+      addToCart,
+      removeFromCart,
+      increaseQuantity,
+      decreaseQuantity,
+      changeBrand
+    }}>
+      {children}
+    </context.Provider>
+  )
+
 }
 
 export default AppState;
