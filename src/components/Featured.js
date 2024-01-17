bd71af282d1e187a5a753ef67e820448630c8f2f
Sijad Khan <86185832+Sijadkhan1678@users.noreply.github.com>
2024-01-17 19:58:06 +0500

    added icon to Featured component

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import { Box, Stack, Typography, Container } from '@mui/material'
+import { Box, Stack, Typography, Container, IconButton } from '@mui/material'
+import DoneIcon from '@mui/icons-material/Done';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
+import PhoneIcon from '@mui/icons-material/Phone';
+
+
 
 
 const Featured = () => {
+  const createContent = (icon, content) => {
 
-  const FeaturedContents = ['Quality Product', 'Free Shipping', '14-Day Return', '24/7 Support']
+    return { icon, content }
+
+  }
+
+  const FeaturedContents = [createContent(<DoneIcon  sx={{ fontSize: '2rem',color:'orange' }} />, 'Quality Product'), createContent(<LocalShippingIcon sx={{ fontSize: '2rem',color:'orange' }} />, 'Free Shipping'), createContent(<CompareArrowsIcon  sx={{ fontSize: '2rem',color:'orange' }} />, '14-Day Return'), createContent(<PhoneIcon sx={{ fontSize: '2rem',color:'orange' }} />, '24/7 Support')]
+
 
   return (
     <Container>
       <Stack direction={{ lg: 'row', md: 'row', sm: 'row' }}
-             mr={{ lg: "4rem", md: '0rem' }}
-             mx={{ lg: "1rem", md: "1.4rem", sm: '1.4rem' }} mb="3rem"
-             flexWrap={{ lg: 'nowrap', md: 'nowrap', sm: 'wrap', xs: 'wrap' }}>
+        mr={{ lg: "4rem", md: '0rem' }}
+        mx={{ lg: "1rem", md: "1.4rem", sm: '1.4rem' }} mb="3rem"
+        flexWrap={{ lg: 'nowrap', md: 'nowrap', sm: 'wrap', xs: 'wrap' }}>
 
-        {FeaturedContents.map((content, index) => (
+        {FeaturedContents.map((feature, index) => {
 
-          <Box key={index} px="15px"
+          return (<Box key={index} px="15px"
 
             flex={{
               lg: '0 0 23%',
@@ -24,14 +36,19 @@ const Featured = () => {
               xs: '0 0 100%'
             }}>
 
-            <Box mb={{ sm: "1.3rem", xs: '1rem' }} p="30px" border="1px solid #EDF1FF">
+            <Stack direction='row' alignItems='center' spacing={1} mb={{ sm: "1.3rem", xs: '1rem' }} p="30px" border="1px solid #EDF1FF">
 
-              <Typography variant='h5' color="#1C1C1C" fontSize="1.25rem" fontWeight={600}>
-                {content}
+              {feature.icon}
+
+              <Typography variant='h5' color="#1C1C1C" fontSize="1.25rem" textAlign='justify' fontWeight={600}>
+                {feature.content}
               </Typography>
-            </Box>
+
+            </Stack>
+
           </Box>
-        ))
+          )
+        })
 
         }
       </Stack>
