78100383f4510d67dac56f5db39c5dc8b8189542
Sijad Khan <86185832+Sijadkhan1678@users.noreply.github.com>
2024-01-31 15:21:58 +0500

    Header component layout change

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { Box, Stack, Badge, IconButton, SwipeableDrawer, List, ListItem, ListItemText } from '@mui/material'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-// import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import logo from './logo.jpg'
 import { NavLink } from 'react-router-dom'
@@ -10,23 +9,21 @@ import context from '../../context/AppContext'
 const Header = () => {
 
     const { cart } = useContext(context)
-    const [open, setOpen] = useState(true)
+    const [open, setOpen] = useState(false)
     const toggleDrawer = () => setOpen(!open)
 
     return (
 
-        <Box p={3} px={{ xs: 2, sm: 4, md: 8, lg: 8 }}>
-            <SwipeableDrawer open={open} onClose={toggleDrawer}>
+        <Box py={5} px={{ xs: 2, sm: 4, md: 8, lg: 10 }}>
+            <SwipeableDrawer open={open} onOpen={toggleDrawer} onClose={toggleDrawer}>
 
                 <Box
                     sx={{ width: 200 }}
                     role="presentation"
                     onClick={toggleDrawer}
-
                 //   onKeyDown={toggleDrawer}
                 >
                     <List>
-
                         <ListItem> <NavLink to='/' style={style}><ListItemText primary='Home' /></NavLink></ListItem>
                         <ListItem><NavLink to='/brands' style={style}><ListItemText primary='Brand' /></NavLink></ListItem>
                         <ListItem><NavLink to='/about' style={style}><ListItemText primary='About' /></NavLink></ListItem>
@@ -44,11 +41,12 @@ const Header = () => {
                     aria-label="menu"
                     onClick={toggleDrawer}
                     sx={{
-                        display: { md: 'none', sm: 'none' }, // Hide on md and larger screens
+                        display: { md: 'none', sm: 'none' }, 
                         mr: 2,
-                    }}
-                >
+                    }}>
+
                     <MenuIcon />
+                    
                 </IconButton>
 
                 <Box width='50px' marginTop="6px">
@@ -56,12 +54,10 @@ const Header = () => {
                 </Box>
 
                 <Stack direction='row' sx={{
-                    display: { xs: 'none', sm: 'flex' }, // Hide on xs, show on sm and above
-                    // Adjust spacing for different screen sizes
+                    justifyContent: 'space-between',
+                    display: { xs: 'none', sm: 'flex' },
                     width: { sm: '50%', md: '30%', lg: '30%' },
-                    ml: { sm: "0", md: "24rem", lg: "24rem" }
-                }}
-                    justifyContent='space-between' >
+                }}>
 
                     <NavLink to='/' style={style}>Home</NavLink>
                     <NavLink to='/brands' style={style}>Brand</NavLink>
