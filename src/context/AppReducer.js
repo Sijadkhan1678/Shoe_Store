3ee4ff2f9de97eea45771c704145cc98285655d0
Sijad Khan <86185832+Sijadkhan1678@users.noreply.github.com>
2024-02-03 19:52:53 +0500

    Code Reformat

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -2,58 +2,60 @@ import { GET_BRAND_PRODUCTS, ADD_TO_CART, REMOVE_FROM_CART, INCREASE_QUANTITY, D
 
 const AppReducer = (state, action) => {
 
-  state.cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+    state.cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
 
-  switch (action.type) {
+    switch (action.type) {
 
-    case GET_BRAND_PRODUCTS:
+        case GET_BRAND_PRODUCTS:
 
-      return {
-        ...state, brandProducts: state.products.filter(product => product.brand === action.payload),
-        selectedBrand: action.payload
-      }
+            return {
+                ...state, brandProducts: state.products.filter(product => product.brand === action.payload),
+                selectedBrand: action.payload
+            }
 
-    case ADD_TO_CART:
+        case ADD_TO_CART:
 
-      const product = state.cart.find( product => product.id === action.payload.id);
-      
-      if (!product) { 
+            const product = state.cart.find(product => product.id === action.payload.id);
+
+            if (!product) {
+
+                return { ...state, cart: [...state.cart, { ...action.payload }] }
+
+            }
+
+            else {
+
+                product.quantity++
+
+            }
+
+        case REMOVE_FROM_CART:
+            
+            return { ...state, cart: state.cart.filter(item => item.id !== action.payload) }
+
+        case INCREASE_QUANTITY:
+
+            return {
+                
+                ...state, cart: state.cart.map(product => product.id !== action.payload ? product : { ...product, quantity: product.quantity + 1 })
+
+            }
+        case DECREASE_QUANTITY:
+
+            if (action.payload.quantity > 1)
+
+                return {
+                    ...state, cart: state.cart.map(product => product.id !== action.payload.id ? product : { ...product, quantity: action.payload.quantity - 1 })
+                }
         
-        return { ...state, cart: [...state.cart, { ...action.payload }] }
+        case CHANGE_BRAND:
+            return {
+                ...state, selectedBrand: action.payload
+            }
 
-      }
-      
-      else {
-
-        product.quantity++  
-
-      }
-
-    case REMOVE_FROM_CART:
-      return { ...state, cart: state.cart.filter(item => item.id !== action.payload) }
-      
-    case INCREASE_QUANTITY:
-
-      return {
-        ...state, cart: state.cart.map(product => product.id !== action.payload ? product : { ...product, quantity: product.quantity + 1 })
-
-      }
-    case DECREASE_QUANTITY:
-
-      if (action.payload.quantity > 1)
-
-        return {
-          ...state, cart: state.cart.map(product => product.id !== action.payload.id ? product : { ...product, quantity: action.payload.quantity - 1 })
-        }
-
-    case CHANGE_BRAND:
-      return {
-        ...state, selectedBrand: action.payload
-      }
-
-    default:
-      return state
-  }
+        default:
+            return state
+    }
 
 }
 
