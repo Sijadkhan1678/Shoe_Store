4a100b0643b1853a45b44d710c75e3fa691b4b7c
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-03-06 09:32:12 +0500

    Code refactor of ProductModal

--- src/components/ProductModal.js
+++ src/components/ProductModal.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Box, Stack, Modal, Grid, Typography, ButtonGroup, Button, useMediaQuery } from '@mui/material'
+import { Box, Modal, Grid, Typography, ButtonGroup, Button, useMediaQuery } from '@mui/material'
 import context from '../context/AppContext'
 
 
 const ProductModal = () => {
 
     const { product, modalOpen, closeModal, addToCart } = React.useContext(context);
-    const [quantity,setQuantity] = React.useState(1);
+    const [quantity, setQuantity] = React.useState(1);
 
     const handleProductQuantity = (type) => {
-          
-        if(type === 'increase'){
-            setQuantity(quantity+1)
+
+        if (type === 'increase') {
+            setQuantity(quantity + 1)
         }
-        if(type === 'decrease'){
-            if(quantity > 1){
-                setQuantity(quantity-1)    
+        if (type === 'decrease') {
+            if (quantity > 1) {
+                setQuantity(quantity - 1)
             }
-            
+
         }
     }
     const handleClose = () => { closeModal(false); setQuantity(1) }
@@ -61,57 +61,59 @@ const ProductModal = () => {
                 <Grid container>
 
                     <Grid item lg={6} md={6} sm={6} xs={12}>
-                       
-                        <img src={product && product.img} style={imageStyle} alt='product' />
-                        
+                        {product && (
+                            <img src={product.img} style={imageStyle} alt={product.name} />
+                        )}
+
                     </Grid>
 
                     <Grid item lg={6} md={6} sm={6} xs={12}>
                         <Box p={2.5}>
 
-                            <Typography variant='h4' component='h3' mb={1.3} fontSize={30} fontWeight={500}>{product && product.name}</Typography>
-                            <Typography variant='h6' fontWeight={300} component='h4'>${product && product.price} USD</Typography>
+                            {product && (<Typography variant='h4' component='h3' mb={1.3} fontSize={30} fontWeight={500}>{product.name}</Typography>)}
+                            {product && (<Typography variant='h6' fontWeight={300} component='h4'>${product.price} USD</Typography>)}
 
                             <Box mt={2}>
-                                <Stack>
-                                    <ButtonGroup>
-                                        <Button onClick={()=>handleProductQuantity('decrease')} sx={{
-                                            variant: 'contained',
-                                            backgroundColor: '#f7f7f7',
-                                            color: '#696969',
-                                            width: '12%',
-                                            border: 'none',
-                                            py: 1.2,
-                                            px: 3,
-                                            '&:hover': { border: 'none', bgcolor: '#e5e5e5' }
 
-                                        }}>-</Button>
-                                        <Button sx={{
-                                            variant: 'contained',
-                                            backgroundColor: '#f7f7f7',
-                                            color: '#696969',
-                                            width: '12%',
-                                            px: 3,
-                                            py: 1.2,
-                                            border: 'none',
-                                            '&:hover': { border: 'none' }
+                                <ButtonGroup>
+                                    <Button onClick={() => handleProductQuantity('decrease')} sx={{
+                                        variant: 'contained',
+                                        backgroundColor: '#f7f7f7',
+                                        color: '#696969',
+                                        width: '12%',
+                                        border: 'none',
+                                        py: 1.2,
+                                        px: 3,
+                                        '&:hover': { border: 'none', bgcolor: '#e5e5e5' }
 
-                                        }}>{quantity}</Button>
-                                        <Button onClick={() => handleProductQuantity('increase')} sx={{
-                                            variant: 'contained',
-                                            backgroundColor: '#f7f7f7',
-                                            color: '#696969',
-                                            py: 1.2,
-                                            px: 3,
-                                            width: '12%',
-                                            border: 'none',
-                                            '&:hover': { border: 'none', bgcolor: '#e5e5e5' }
+                                    }}>-</Button>
+                                    <Button disableRipple sx={{
+                                        variant: 'contained',
+                                        backgroundColor: '#f7f7f7',
+                                        color: '#696969',
+                                        cursor: 'default',
+                                        width: '12%',
+                                        px: 3,
+                                        py: 1.2,
+                                        border: 'none',
+                                        '&:hover': { border: 'none', bgcolor: '#f7f7f7' }
 
-                                        }}>+</Button>
-                                    </ButtonGroup>
+                                    }}>{quantity}</Button>
+                                    <Button onClick={() => handleProductQuantity('increase')} sx={{
+                                        variant: 'contained',
+                                        backgroundColor: '#f7f7f7',
+                                        color: '#696969',
+                                        py: 1.2,
+                                        px: 3,
+                                        width: '12%',
+                                        border: 'none',
+                                        '&:hover': { border: 'none', bgcolor: '#e5e5e5' }
 
-                                </Stack>
-                                <Button fullWidth onClick={() => addToCart({...product,quantity},'modal_view')} variant="contained" size="large" sx={{
+                                    }}>+</Button>
+                                </ButtonGroup>
+
+
+                                <Button fullWidth onClick={() => addToCart({ ...product, quantity }, 'modal_view')} variant="contained" size="large" sx={{
                                     color: "white",
                                     bgcolor: "#524938",
                                     '&:hover': { bgcolor: '#ff7800' },
