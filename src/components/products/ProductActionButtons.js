38cf8056db8c666cb342ec613a6c11ccd3bfdc26
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-03-10 16:06:16 +0500

    Code refactor

--- src/components/products/ProductActionButtons.js
+++ src/components/products/ProductActionButtons.js
@@ -5,7 +5,7 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import Appcontext from '../../context/AppContext';
 
 
-const ProductActionButtons = ({  product, isHovered  }) => {
+const ProductActionButtons = ({ product, isHovered }) => {
 
     const { addToCart, addProduct, openModal } = useContext(Appcontext);
 
@@ -14,55 +14,55 @@ const ProductActionButtons = ({  product, isHovered  }) => {
         addProduct(product)
         openModal()
     }
+    const defaultStyle = {
+        variant: 'contained',
+        bgcolor: '#524938',
+        color: 'white',
+        border: 'none',
+    }
+    const hoverStyled = {
+        transform: isHovered ? 'translateY(-24px)' : 'translateY(-10px)',
+        visibility: isHovered ? 'visible' : 'hidden',
+        opacity: isHovered ? '1' : '0',
+        '&:hover': { bgcolor: '#ff7800', border: 'none' }
+    }
+    const addDefaultStyle = (style) => {
+
+        return { ...defaultStyle, ...style, ...hoverStyled }
+
+    }
+    const quickViewButtonStyle = addDefaultStyle({
+        width: '20%',
+        transition: 'opacity 300ms ease-in, transform 300ms ease-in, visibility 300ms ease-in',
+    })
+    const addToCartButtonStyle = addDefaultStyle({
+        width: '56%',
+        borderLeft: '1px solid white',
+        transition: 'opacity 400ms ease-in, transform 400ms ease-in-out, visibility 400ms ease-in',
+        transitionDelay: '200ms',
+    })
+    const compareButtonStyle = addDefaultStyle({
+        width: '20%',
+        borderLeft: '1px solid white',
+        transition: 'opacity 600ms ease-in, transform 600ms ease-in, visibility 600ms ease-in',
+        transitionDelay: '200ms',
+
+    })
 
     return (
 
         <ButtonGroup orientation="horizontal" size="large"
-            sx={{ width: '100%', ml: '9px', transform: 'translateY(-20px)' }}
-        >
-            <Button onClick={handleViewModal} startIcon={<VisibilityIcon />} sx={{
-                variant: 'contained',
-                bgcolor: '#524938',
-                color: 'white',
-                width: '20%',
-                border: 'none',
-                transform: isHovered ? 'translateY(-24px)' : 'translateY(-10px)',
-                transition: 'opacity 300ms ease-in,transform 300ms ease-in,visibility 300ms ease-in',
-                visibility: isHovered ? 'visible' : 'hidden',
-                opacity: isHovered ? '1' : '0',
-                '&:hover': { bgcolor: '#ff7800', border: 'none' }
-            }} />
-            <Button onClick={() => addToCart(product)}
+            sx={{
+                width: '100%',
+                ml: '9px',
+                transform: 'translateY(-20px)'
+            }}>
 
-                sx={{
-                    variant: 'contained',
-                    bgcolor: '#524938',
-                    color: 'white',
-                    width: '56%',
-                    border: 'none',
-                    borderLeft: '1px solid white',
-                    transform: isHovered ? 'translateY(-24px)' : 'translateY(-10px)',
-                    transition: 'opacity 400ms ease-in, transform 400ms ease-in-out, visibility 400ms ease-in',
-                    transitionDelay: '200ms',
-                    visibility: isHovered ? 'visible' : 'hidden',
-                    opacity: isHovered ? '1' : '0',
-                    '&:hover': { bgcolor: '#ff7800', border: 'none' }
+            <Button onClick={handleViewModal} startIcon={<VisibilityIcon />} sx={quickViewButtonStyle} />
 
-                }}>ADD TO CART</Button>
-            <Button sx={{
-                variant: 'contained',
-                backgroundColor: '#524938',
-                color: 'white',
-                width: '20%',
-                border: 'none',
-                borderLeft: '1px solid white',
-                transform: isHovered ? 'translateY(-24px)' : 'translateY(-10px)',
-                transition: 'opacity 600ms ease-in,transform 600ms ease-in,visibility 600ms ease-in',
-                visibility: isHovered ? 'visible' : 'hidden',
-                transitionDelay: '200ms',
-                opacity: isHovered ? '1' : '0',
-                '&:hover': { bgcolor: '#ff7800', border: 'none' }
-            }} startIcon={<CompareArrowsIcon />} />
+            <Button onClick={() => addToCart(product)} sx={addToCartButtonStyle}>ADD TO CART</Button>
+
+            <Button sx={compareButtonStyle} startIcon={<CompareArrowsIcon />} />
 
         </ButtonGroup>
     )
