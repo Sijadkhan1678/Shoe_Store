94a89215264ca0eaee49b37120e9bc7ed2092ad8
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-03-21 22:38:28 +0500

    Add input field

--- src/components/pages/Checkout.js
+++ src/components/pages/Checkout.js
@@ -1,9 +1,47 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Grid, FormControl, Input, InputBase, TextField } from '@mui/material'
 
 const Checkout = () => {
   return (
-    <Box>Checkout</Box>
+
+    <Box>
+
+      <FormControl variant='standard'>
+
+        <Grid container spacing={4}>
+
+          <Grid item>
+            <TextField
+              id="first"
+              label="First Name"
+            // value={first}
+            // onChange={e => setFirst(e.target.value)}
+            />
+          </Grid>
+
+          <Grid item>
+            <TextField
+              id="lastname"
+              label="Last Name"
+            // value={second}
+            // onChange={e => setSecond(e.target.value)}
+            />
+          </Grid>
+
+          <Grid item>
+            <TextField
+              id="email"
+              label="Email"
+            // value={third}
+            // onChange={e => setThird(e.target.value)}
+            />
+          </Grid>
+
+        </Grid>
+
+      </FormControl>
+
+    </Box>
   )
 }
 
