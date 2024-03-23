67e9556d2bba066b49d9b4d343eae1b235fd8cb2
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-03-23 19:40:19 +0500

    complete billing adddress and add cart summary container layout

--- src/components/pages/Checkout.js
+++ src/components/pages/Checkout.js
@@ -1,83 +1,152 @@
 import React from 'react'
-import { Container, Box, Typography,Grid, FormControl, Input, InputBase, TextField } from '@mui/material'
+import { Container, Box, Typography, Grid, Stack,FormControl, Input, InputBase, TextField } from '@mui/material'
 
 const Checkout = () => {
+
   return (
-    
 
-     <Box bgcolor='whitesmoke'>
-       <Container >
+    <Box p={3} >
 
-      <Box bgcolor='white' >
+      <Container >
+        {/* shipping */}
+        <Stack direction={{lg:'row',md:'row',sm:'column'}} gap={3}>
+
+        <Box width='70%'>  
+
+        <Box p={3} bgcolor='yellow'  >
+
+          <Box py={1}>
+
+            <Typography variant='h6' component='h4' sx={{ fontSize: '14px', color: '#2c2c2c' }} fontWeight='600'>Shipping Address</Typography>
+
+          </Box>
+
+          <Grid container spacing={4}>
+
+            <Grid item lg={6}  >
+
+              {/* <FormControl variant='standard'> */}
+              <Grid item mt={2}>
+
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="fullname"
+                  label="Full Name"
+                // value={first}
+                // onChange={e => setFirst(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="phoone"
+                  label="Phone Number"
+
+                // value={second}
+                // onChange={e => setSecond(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="zip"
+                  label="Zip Code"
+
+                // value={third}
+                // onChange={e => setThird(e.target.value)}
+                />
 
 
-      <Typography variant='h6' component='h3'>Shipping Address</Typography>
-        <Grid container  spacing={4}>
-          
+              </Grid>
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="address"
+                  label="Address 1"
+
+                // value={third}
+                // onChange={e => setThird(e.target.value)}
+                />
 
 
+              </Grid>
 
-          {/* shipping */}
-          <Grid item lg={4}  >
+              {/* </FormControl> */}
 
-            {/* <FormControl variant='standard'> */}
-            <Grid item>
-
-              <TextField
-                size='small'
-                fullWidth
-                id="fullname"
-                label="Full Name"
-              // value={first}
-              // onChange={e => setFirst(e.target.value)}
-              />
+              
             </Grid>
 
-            <Grid item>
-              <TextField
-                size='small'
-                fullWidth
-                id="phoone"
-                label="Phone Number"
 
-              // value={second}
-              // onChange={e => setSecond(e.target.value)}
-              />
+
+
+
+            <Grid item lg={6}>
+
+
+              <Grid item mt={2}>
+
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                // value={first}
+                // onChange={e => setFirst(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="company"
+                  label="Company"
+
+                // value={second}
+                // onChange={e => setSecond(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="country"
+                  label="Country"
+
+                // value={third}
+                // onChange={e => setThird(e.target.value)}
+                />
+
+
+              </Grid>
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="address"
+                  label="Address 2"
+
+                // value={third}
+                // onChange={e => setThird(e.target.value)}
+                />
+
+
+              </Grid>
+
             </Grid>
-
-            <Grid item>
-              <TextField
-                size='small'
-                fullWidth
-                id="zip"
-                label="Zip Code"
-
-              // value={third}
-              // onChange={e => setThird(e.target.value)}
-              />
-
-
-            </Grid>
-            <Grid item>
-              <TextField
-                size='small'
-                fullWidth
-                id="address"
-                label="Address 1"
-
-              // value={third}
-              // onChange={e => setThird(e.target.value)}
-              />
-
-
-            </Grid>
-
-            {/* </FormControl> */}
 
 
           </Grid>
 
-          {/* shipping end */}
+        </Box>
+        {/* shipping end */}
 
 
 
@@ -90,81 +159,168 @@ const Checkout = () => {
 
 
 
+        {/* Billing Addresss */}
+        <Box p={3} bgcolor='yellow'  mt={4} >
+          <Box py={1}>
+
+            <Typography variant='h6' component='h4' sx={{ fontSize: '14px', color: '#2c2c2c' }} fontWeight='600'>Shipping Address</Typography>
+
+          </Box>
+
+          <Grid container spacing={4}>
 
 
 
 
 
+            <Grid item lg={6}  >
+
+              {/* <FormControl variant='standard'> */}
+              <Grid item mt={2}>
+
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="fullname"
+                  label="Full Name"
+                // value={first}
+                // onChange={e => setFirst(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="phoone"
+                  label="Phone Number"
+
+                // value={second}
+                // onChange={e => setSecond(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="zip"
+                  label="Zip Code"
+
+                // value={third}
+                // onChange={e => setThird(e.target.value)}
+                />
 
 
+              </Grid>
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="address"
+                  label="Address 1"
+
+                // value={third}
+                // onChange={e => setThird(e.target.value)}
+                />
 
 
+              </Grid>
+
+              {/* </FormControl> */}
 
 
-          {/* Billing Address */}
-          <Grid item spacing={2} lg={4}>
-
-
-            <Grid item>
-
-              <TextField
-                size='small'
-                fullWidth
-                id="fullname"
-                label="Full Name"
-              // value={first}
-              // onChange={e => setFirst(e.target.value)}
-              />
             </Grid>
 
-            <Grid item>
-              <TextField
-                size='small'
-                fullWidth
-                id="phoone"
-                label="Phone Number"
 
-              // value={second}
-              // onChange={e => setSecond(e.target.value)}
-              />
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <Grid item spacing={2} lg={6}>
+
+
+              <Grid item mt={2}>
+
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                // value={first}
+                // onChange={e => setFirst(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="company"
+                  label="Company"
+
+                // value={second}
+                // onChange={e => setSecond(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="country"
+                  label="Country"
+
+                // value={third}
+                // onChange={e => setThird(e.target.value)}
+                />
+
+
+              </Grid>
+              <Grid item mt={2}>
+                <TextField
+                  size='small'
+                  fullWidth
+                  id="address"
+                  label="Address 2"
+
+                // value={third}
+                // onChange={e => setThird(e.target.value)}
+                />
+
+
+              </Grid>
+
             </Grid>
 
-            <Grid item>
-              <TextField
-                size='small'
-                fullWidth
-                id="zip"
-                label="Zip Code"
-
-              // value={third}
-              // onChange={e => setThird(e.target.value)}
-              />
-
-
-            </Grid>
-            <Grid item>
-              <TextField
-                size='small'
-                fullWidth
-                id="address"
-                label="Address 1"
-
-              // value={third}
-              // onChange={e => setThird(e.target.value)}
-              />
-
-
-            </Grid>
 
           </Grid>
 
+        </Box>
+        </Box>
+        <Box width="30%" bgcolor='red'>
+          Cart Summary
+        </Box>
+        
+        </Stack>
+        {/* Billing Address end */}
 
-        </Grid>
 
-      </Box>
+        {/* Start Cart Summary  */}
+
+        
       </Container>
-      </Box>
-    
+    </Box>
+
   )
 }
 
