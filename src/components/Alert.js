130bbe534c5bfc37ace49cc63eb3ddea7051ba71
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-05-18 09:35:32 +0500

    design completed of Alert and connect AlertContext with Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import { Alert } from '@mui/material'
+import context from '../context/alert/Context'
 
 
 const CartAlert = () => {
-  const [topPosition, setTopPosition] = React.useState('-60px')
+
+  const { message, removeAlert } = React.useContext(context)
+
+
   React.useEffect(() => {
-    window.addEventListener('scroll', () => {
-      setTopPosition((window.scrollY - 60) + 'px')
 
-    })
+    message && removeAlert()
 
-  }, [])
+  }, [removeAlert, message])
+
 
   return (
-    <Alert variant="filled" severity="success" sx={{ maxWidth: 300, transition: 'top 300ms ease-in-out', position: 'absolute', left: 0, right: 0, top: `${topPosition}`, mx: 'auto', zIndex: 100 }}>
-      Successfully Add To Cart
+
+    <Alert variant="filled" severity="success" sx={{
+      maxWidth: 300,
+      transition: 'all 300ms ease-in-out',
+      position: 'fixed',
+      left: 0,
+      right: 0,
+      top: message !== null ? 20 : -50,
+      mx: 'auto',
+      zIndex: 100,
+      // display: alert === null ? 'block' : 'none'
+    }}>
+
+      {message}
+
     </Alert>
+
   )
 }
 
