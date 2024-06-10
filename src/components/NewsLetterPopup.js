2a9b5568f02603fef75b963c97398dd8650bf45a
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-06-10 19:39:33 +0500

    Add styling and change image

--- src/components/NewsLetterPopup.js
+++ src/components/NewsLetterPopup.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Dialog, Grid, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField, Button } from '@mui/material'
-import newLetter from './newsletter.webp'
+import { Dialog, Grid, Box, DialogTitle, DialogContent, DialogContentText, TextField, Button } from '@mui/material'
+
+import newLetter from './newsletter-img.webp'
 const NewsLetterPopup = () => {
 
     const [open, setOpen] = React.useState(true)
@@ -15,33 +16,48 @@ const NewsLetterPopup = () => {
             open={open}
             onClose={handleClose}
             aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description">
-
+            aria-describedby="alert-dialog-description"
+            fullWidth
+            maxWidth='xl'>
             <Grid container>
-                <Grid item lg={6}>
-                    <img src={newLetter} alt='newsLetter' width='100%' height='500' />
+                <Grid item lg={6} p={2.5}>
+                    <img src={newLetter} alt='newsLetter' width='100%' height='450' />
                 </Grid>
 
-                <Grid item lg={6}>
-                    <DialogTitle id="alert-dialog-title">
-                        {"Get Discount 30% Off"}
-                    </DialogTitle>
+                <Grid item lg={6} p={3}>
+                    <Box mt={3}>
+                        <DialogTitle id="alert-dialog-title" sx={{ fontSize: 36, fontWeight: 700 }}>
+                            "Get Discount <span style={{ color: 'red' }}>30%</span> Off"
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description" sx={{ mb: 4, fontSize: 16, fontWeight: 400, }}>
+                                '  Sign up to our newsletter and save 30% for you next purchase. No spam, we promise!'
+                            </DialogContentText>
+                            <TextField fullWidth
+                                required
+                                margin='desne'
+                                id='email'
+                                type='email'
+                                label="Your Email"
+                                sx={{ bgcolor: "#f4f4f4" }} />
+                        </DialogContent>
+                        <Button
+                            variant="contained"
+                            type='submit'
+                            size="large"
+                            sx={{
+                                color: "white",
+                                bgcolor: "#524938",
+                                mt: '30px',
+                                mb: '10px',
+                                p: '15px 55px',
+                                mx: 'auto',
+                                display: 'block',
+                                borderRadius: '50px',
+                                '&:hover': { bgcolor: '#ff7800' },
+                            }}>Subscribe</Button>
 
-                    <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            '  Sign up to our newsletter and save 30% for you next purchase. No spam, we promise!'
-                        </DialogContentText>
-                    </DialogContent>
-
-                    <DialogActions>
-                    <Button variant="contained" size="large" sx={{
-                            color: "white",
-                            bgcolor: "#524938",
-                            '&:hover': { bgcolor: '#ff7800' },
-                            display: 'block',
-                            borderRadius: '5px'
-                        }}>Subscribe Now</Button>
-                    </DialogActions>
+                    </Box>
                 </Grid>
 
             </Grid>
