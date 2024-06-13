1791de7c84be7d0df67abe53d561a7a1fab7f2ab
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-06-13 09:28:26 +0500

    Add FormHelperText,checkBox and style modify

--- src/components/NewsLetterPopup.js
+++ src/components/NewsLetterPopup.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Dialog, Grid, Box, DialogTitle, DialogContent, DialogContentText, TextField, Button } from '@mui/material'
-
+import { Dialog, Grid, Box, Stack,DialogTitle, DialogContent, DialogContentText, TextField, Button, FormHelperText, Checkbox } from '@mui/material'
 import newLetter from './newsletter-img.webp'
+
 const NewsLetterPopup = () => {
 
     const [open, setOpen] = React.useState(true)
@@ -21,25 +21,28 @@ const NewsLetterPopup = () => {
             maxWidth='xl'>
             <Grid container>
                 <Grid item lg={6} p={2.5}>
-                    <img src={newLetter} alt='newsLetter' width='100%' height='450' />
+                    <img src={newLetter} alt='newsLetter' width='100%' height='490' />
                 </Grid>
 
-                <Grid item lg={6} p={3}>
-                    <Box mt={3}>
-                        <DialogTitle id="alert-dialog-title" sx={{ fontSize: 36, fontWeight: 700 }}>
+                <Grid item lg={6} pr={4}>
+                    <Box mt={6}>
+                        <DialogTitle id="alert-dialog-title" sx={{ textAlign: 'center', fontSize: 36, fontWeight: 700 }}>
                             "Get Discount <span style={{ color: 'red' }}>30%</span> Off"
                         </DialogTitle>
                         <DialogContent>
-                            <DialogContentText id="alert-dialog-description" sx={{ mb: 4, fontSize: 16, fontWeight: 400, }}>
+                            <DialogContentText id="alert-dialog-description" sx={{ mb: 4, textAlign: 'center', fontSize: 16, fontWeight: 400, }}>
                                 '  Sign up to our newsletter and save 30% for you next purchase. No spam, we promise!'
                             </DialogContentText>
-                            <TextField fullWidth
-                                required
+                            <TextField
+                                fullWidth
                                 margin='desne'
                                 id='email'
                                 type='email'
-                                label="Your Email"
-                                sx={{ bgcolor: "#f4f4f4" }} />
+                                label="Enter Your Email"
+                                sx={{ bgcolor: "#f4f4f4", color: '#222', borderRadius: '100' }}
+
+                            />
+                            <FormHelperText sx={{ color: 'red', mt: 1 }}>please enter your email</FormHelperText>
                         </DialogContent>
                         <Button
                             variant="contained"
@@ -49,15 +52,18 @@ const NewsLetterPopup = () => {
                                 color: "white",
                                 bgcolor: "#524938",
                                 mt: '30px',
-                                mb: '10px',
+                                mb: '60px',
                                 p: '15px 55px',
                                 mx: 'auto',
                                 display: 'block',
                                 borderRadius: '50px',
                                 '&:hover': { bgcolor: '#ff7800' },
                             }}>Subscribe</Button>
-
+                        <Stack direction='row' alignItems='center' justifyContent='center'>
+                            <Checkbox sx={{ color: 'red', }} /> <FormHelperText>don`t show again`</FormHelperText>
+                        </Stack>
                     </Box>
+
                 </Grid>
 
             </Grid>
