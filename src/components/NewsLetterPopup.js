bc7717f940b2a1dbc6a55feebfa918d84bd1d26d
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-06-17 16:07:18 +0500

    Add Mobile Responsiveness

--- src/components/NewsLetterPopup.js
+++ src/components/NewsLetterPopup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dialog, Grid, useMediaQuery,Box, Stack, DialogTitle, DialogContent, DialogContentText, TextField, Button, FormHelperText, Checkbox } from '@mui/material'
+import { Dialog, Grid, useMediaQuery, Box, Stack, DialogTitle, DialogContent, DialogContentText, TextField, Button, FormHelperText, Checkbox } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close';
 import newLetter from './newsletter-img.webp'
 
@@ -16,9 +16,9 @@ const NewsLetterPopup = () => {
         setClose(false)
     }
     const imgStyle = {
-        
-         display:(isLarge? 'block':(isMedium? 'none':(isSmall?'none': isMobile?'none':'block')))
-    } 
+
+        display: (isLarge ? 'block' : (isMedium ? 'none' : (isSmall ? 'none' : isMobile ? 'none' : 'block')))
+    }
 
     return (
         <Dialog
@@ -29,29 +29,52 @@ const NewsLetterPopup = () => {
             fullWidth
             maxWidth='xl'>
             <Grid container>
-                <Grid item lg={6}  p={2.5}>
-                    hello
-                    <img src={newLetter} alt='newsLetter' style={imgStyle} width='100%' height='490' />
+                <Grid item lg={6} md={6} p={2.5} sx={{
+                    display: {
+                        md: 'block',
+                        sm: 'none',
+                        xs: 'none'
+                    }
+                }}>
+                    <img src={newLetter} alt='newsLetter' width='100%' height={490} />
                 </Grid>
 
-                <Grid item lg={6} sm={12} pr={4}>
-                    <Box mt={6}>
-                        <DialogTitle id="alert-dialog-title" sx={{ textAlign: 'center', fontSize: 36, fontWeight: 700 }}>
+                <Grid item lg={6} md={6} sm={12} pr={{ md: 4, xs: 0 }}>
+                    <Box mt={{ md: 6, xs: 2 }}>
+                        <DialogTitle id="alert-dialog-title" sx={{
+                            textAlign: 'center',
+                            fontSize: { md: 36, xs: 27 },
+                            fontWeight: { md: 700, xs: 600 }
+                        }}>
                             "Get Discount <span style={{ color: 'red' }}>30%</span> Off"
                         </DialogTitle>
                         <DialogContent>
-                            <DialogContentText id="alert-dialog-description" sx={{ mb: 4, textAlign: 'center', fontSize: 16, fontWeight: 400, }}>
-                                '  Sign up to our newsletter and save 30% for you next purchase. No spam, we promise!'
+                            <DialogContentText id="alert-dialog-description" sx={{
+                                mb: 4,
+                                textAlign: 'center',
+                                fontSize: 16,
+                                fontWeight: 400,
+                            }}>
+                                'Sign up to our newsletter and save 30% for you next purchase. No spam, we promise!'
                             </DialogContentText>
-                            <TextField
-                                fullWidth
-                                margin='desne'
-                                id='email'
-                                type='email'
-                                label="Enter Your Email"
-                                sx={{ bgcolor: "#f4f4f4", color: '#222', borderRadius: '100' }}
+                            <Box >
+                                <TextField
+                                    fullWidth
+                                    // margin='dense'
+                                    // height='10px'
+                                    size='small'
+                                    id='email'
+                                    type='email'
+                                    label="Enter Your Email"
+                                    sx={{
+                                        bgcolor: "#f4f4f4",
+                                        color: '#222',
+                                        borderRadius: '100',
+                                        fontSize: 8
+                                    }}
 
-                            />
+                                />
+                            </Box>
                             <FormHelperText sx={{ color: 'red', mt: 1 }}>please enter your email</FormHelperText>
                         </DialogContent>
                         <Button
@@ -61,34 +84,34 @@ const NewsLetterPopup = () => {
                             sx={{
                                 color: "white",
                                 bgcolor: "#524938",
-                                mt: '30px',
-                                mb: '60px',
-                                p: '15px 55px',
+                                mt: { md: '30px', xs: '10px' },
+                                mb: { md: '60px', xs: '10px' },
+                                p: { md: '15px 55px', xs: '8px 40px' },
                                 mx: 'auto',
                                 display: 'block',
                                 borderRadius: '50px',
                                 '&:hover': { bgcolor: '#ff7800' },
                             }}>Subscribe</Button>
                         <Stack direction='row' alignItems='center' justifyContent='center'>
-                            <Checkbox sx={{ color: 'gray','&:hover':{color:'darkgray'} }} /> <FormHelperText>don`t show again`</FormHelperText>
+                            <Checkbox sx={{ color: 'gray', '&:hover': { color: 'darkgray' } }} />
+                            <FormHelperText>don`t show again`</FormHelperText>
                         </Stack>
                     </Box>
 
                 </Grid>
                 <Box sx={{
                     position: 'absolute',
-                    right: 15,
-                    top: 10,
+                    right: { md: 15, xs: 5 },
+                    top: { md: 10, xs: 5 },
                     textAlign: 'center',
-                    
-                    
+
+
                 }}>
                     <CloseIcon sx={{
                         color: 'gray',
-                        // size: 'small',
                         bgcolor: '#f4f4f4',
-                        fontSize:20,
-                        p: 1.2,
+                        fontSize: { md: 20, xs: 16 },
+                        p: { md: 1.2, xs: 0.9 },
                         borderRadius: '50px'
                     }} />
                 </Box>
