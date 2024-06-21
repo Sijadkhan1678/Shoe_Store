f7ddaba28660d611acae3304cf251e467b395bd5
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-06-21 16:18:03 +0500

    add mobile friendly responsiveness

--- src/components/NewsLetterPopup.js
+++ src/components/NewsLetterPopup.js
@@ -1,5 +1,24 @@
+
+// container px margin -15px
+
+// two sub container (Grid Item) px padding 15px
+
+
+
+// two sub container (Grid Item) px padding 15px
+
+// sub Box right container margin auto
+//  Box=> right => h3 {fontSiz : 36px , marginBottom:30px;fontWeight:700}
+//  Box=> right => p {fontSiz : 1rem , marginBottom:30px;fontWeight:400;Sign up to our newsletter and save 30% for you next purchase. No spam, we promise!}
+
+//  Box=> right => form {fontSiz : 1rem , marginBottom:30px;fontWeight:400}
+/*  Box=> right => form => input => email {border:50px,fontSiz : 14px ,bgcolor:#f4f4f4,
+ marginBottom:30px;fontWeight:400,p:0 14px,lineHeight:46px}
+
+ box=> right => Button => {p:15px 55px,fnt:18px;borRadius:50px;fontWeght:400;}
+ */
 import React from 'react'
-import { Dialog, Grid, useMediaQuery, Box, Stack, DialogTitle, DialogContent, DialogContentText, TextField, Button, FormHelperText, Checkbox } from '@mui/material'
+import { Dialog, Grid, useMediaQuery, Box, Stack, DialogTitle, DialogContent, DialogContentText, TextField, Button, FormHelperText, Checkbox, InputBase } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close';
 import newLetter from './newsletter-img.webp'
 
@@ -13,11 +32,14 @@ const NewsLetterPopup = () => {
     const isLarge = useMediaQuery(`(min-width:1200px)`)
 
     const handleClose = () => {
-        setClose(false)
+        setOpen(!open)
     }
     const imgStyle = {
 
-        display: (isLarge ? 'block' : (isMedium ? 'none' : (isSmall ? 'none' : isMobile ? 'none' : 'block')))
+        // display: (isLarge ? 'block' : ( : (isSmall ? 'none' : isMobile ? 'none' : 'block')))
+        width:'100%',
+        maxHeight:490,
+        height: isMedium && 450
     }
 
     return (
@@ -27,30 +49,32 @@ const NewsLetterPopup = () => {
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
             fullWidth
-            maxWidth='xl'>
+            maxWidth='xl'
+        >
             <Grid container>
-                <Grid item lg={6} md={6} p={2.5} sx={{
+                <Grid item lg={6} md={6} p={{ md: 2.5, xs: 1.5 }} sx={{
                     display: {
                         md: 'block',
                         sm: 'none',
                         xs: 'none'
                     }
                 }}>
-                    <img src={newLetter} alt='newsLetter' width='100%' height={490} />
+                    <img src={newLetter} style={imgStyle} alt='newsLetter'   />
                 </Grid>
 
-                <Grid item lg={6} md={6} sm={12} pr={{ md: 4, xs: 0 }}>
-                    <Box mt={{ md: 6, xs: 2 }}>
+                <Grid item lg={6} md={6} sm={12} xs={12} pr={{ md: 4, xs: 0 }}>
+                    <Box mt={{ md: 6, xs: 0 }}>
                         <DialogTitle id="alert-dialog-title" sx={{
                             textAlign: 'center',
                             fontSize: { md: 36, xs: 27 },
-                            fontWeight: { md: 700, xs: 600 }
+                            fontWeight: { md: 700, xs: 600 },
+                            lineHeight: 1.2
                         }}>
-                            "Get Discount <span style={{ color: 'red' }}>30%</span> Off"
+                            Get Discount <span style={{ color: 'red' }}>30%</span> Off
                         </DialogTitle>
                         <DialogContent>
                             <DialogContentText id="alert-dialog-description" sx={{
-                                mb: 4,
+                                mb: { md: 4, xs: 2.5 },
                                 textAlign: 'center',
                                 fontSize: 16,
                                 fontWeight: 400,
@@ -58,7 +82,10 @@ const NewsLetterPopup = () => {
                                 'Sign up to our newsletter and save 30% for you next purchase. No spam, we promise!'
                             </DialogContentText>
                             <Box sx={{
-                                p: 2,
+                                px: { md: 2, xs: 3 },
+                                py: { md: 2, xs: 1 },
+                                // height:44,
+                                // textAlign:'center',
                                 bgcolor: '#f4f4f4',
                                 borderRadius: '50px'
                             }}>
@@ -70,13 +97,13 @@ const NewsLetterPopup = () => {
                                     placeholder="Enter Your Email"
                                     sx={{
                                         color: '#222',
+                                        // fontSize:16,
                                         // p:2,
                                         borderRadius: '50px'
                                     }}
-
-                                />
+/>
                             </Box>
-                            <FormHelperText sx={{ color: 'red', mt: 1 }}>please enter your email</FormHelperText>
+                            <FormHelperText sx={{ color: 'red', ml: 1, mt: 1.5 }}>please enter your email</FormHelperText>
                         </DialogContent>
                         <Button
                             variant="contained"
@@ -85,17 +112,17 @@ const NewsLetterPopup = () => {
                             sx={{
                                 color: "white",
                                 bgcolor: "#524938",
-                                mt: { md: '30px', xs: '10px' },
-                                mb: { md: '60px', xs: '10px' },
-                                p: { md: '15px 55px', xs: '8px 40px' },
+                                mt: { md: '30px', xs: '0px' },
+                                mb: { lg: '60px',md:'30px', xs: '5px' },
+                                p: { sm: '15px 55px', xs: '8px 40px' },
                                 mx: 'auto',
                                 display: 'block',
                                 borderRadius: '50px',
                                 '&:hover': { bgcolor: '#ff7800' },
                             }}>Subscribe</Button>
                         <Stack direction='row' alignItems='center' justifyContent='center'>
-                            <Checkbox sx={{ color: 'gray', '&:hover': { color: 'darkgray' } }} />
-                            <FormHelperText>don`t show again`</FormHelperText>
+                            <Checkbox value='first' color='primary' size='small' sx={{'&:hover': { color: 'darkgray' } }} />
+                            <FormHelperText>Do not show this popup again`</FormHelperText>
                         </Stack>
                     </Box>
 
