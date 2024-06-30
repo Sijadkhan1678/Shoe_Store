fe864b09dcf9cb6fd28b76ce8abe008a60eea2e1
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-06-30 23:15:41 +0500

    Fix Button Text and typography margin,size

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,77 +9,75 @@ const Banner = () => {
   return (
     <Grid container px={4}>
       <Grid item lg={4} position='relative'>
-        <img src={banner1} alt='banner1' style={{verticalAlign:'middle',width:'100%' ,height:'auto'}} />
+        <img src={banner1} alt='banner1' style={{ border: 'none', verticalAlign: 'middle', width: '100%', maxWidth: '100%', height: 'auto' }} />
         <Box sx={{
           position: 'absolute',
-
-
           bottom: '50px',
           left: 0,
           right: 0,
           // transform:'translateX(-50%)'
 
           // zIndex:100
-          textAlign:'center'
+          textAlign: 'center'
 
         }}>
           <Typography variant='h2' sx={{ fontSize: 50, fontWeight: 700, color: 'white' }}>Run Don't Hide</Typography>
-          <Typography variant='h5' sx={{ fontSize: 24, fontWeight: 400, color: 'white' }}>Nike Flash Be Seen</Typography>
+          <Typography variant='h5' sx={{ fontSize: 20, fontWeight: 400, color: 'white' }}>Nike Flash Be Seen</Typography>
         </Box>
       </Grid>
 
       <Grid item lg={8} position='relative'>
         <Grid item>
-          <img src={banner2} alt='banner1' style={{verticalAlign:'middle',width:'100%' ,height:'auto'}} />
+          <img src={banner2} alt='banner1' style={{ border: 'none', verticalAlign: 'middle', width: '100%', height: 'auto' }} />
           <Box sx={{
             position: 'absolute',
 
             top: '25%',
             right: '20px',
             transform: 'translateY(-50%)',
-          
-          }}>
-          <Typography variant='h5' sx={{ fontSize: 24, fontWeight: 400, color: 'white' }}>Women's Training</Typography>
-          <Typography variant='h2' sx={{ fontSize: 50, fontWeight: 700, color: 'white' }}>Don't Rest Bra</Typography>
-          <Typography variant='body1' sx={{ fontSize: 24, fontWeight: 700, color: 'white',mb:1, }}>Best Sellers</Typography>
-          <Button sx={{bgcolor:'#222',color:'#fff',fontSize:16,fontWeight:400,borderRadius:'50px',p:'10px 50px'}}>More info</Button>
-        </Box>
-      </Grid>
 
-      <Grid container>
-        <Grid item lg={6} position='relative'>
-          <img src={banner3} alt='banner3' style={{verticalAlign:'middle',width:'100%' ,height:'auto'}} />
-          <Box sx={{
-            position: 'absolute',
-            top: '50%',
-            transform:'translateY(-50%)',
-            right:0,
-            left:0,
-            textAlign:'center',
-            px:'30px'
           }}>
-            <Typography variant='h5' sx={{ fontSize: 24, fontWeight: 400, color: '#222' }}>Hello Summer 2019</Typography>
-            <Typography variant='h2' sx={{ fontSize: 50, fontWeight: 700, color: '#222' }}>Extra 50%Off</Typography>
-            <Typography variant='subtitle1'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</Typography>
-            <Button sx={{bgcolor:'#222',color:'#fff',mt:2,fontSize:18,fontWeight:400,borderRadius:'50px',px:'30px'}}>More info</Button>
+            <Typography variant='h5' sx={{ fontSize: 22, fontWeight: 400, color: 'white' }}>Women's Training</Typography>
+            <Typography variant='h2' sx={{ fontSize: 50, fontWeight: 700, color: 'white' }}>Don't Rest Bra</Typography>
+            <Typography variant='body1' sx={{ fontSize: 24, fontWeight: 700, color: 'white', mb: 1, }}>Best Sellers</Typography>
+            <Button sx={{ bgcolor: '#222', color: '#fff', fontSize: 16, textTransform: 'capitalize', fontWeight: 400, borderRadius: '50px', py: '10px', px: '50px' }}>More info</Button>
           </Box>
         </Grid>
-        <Grid item lg={6} position='relative'>
-          <img src={banner4} style={{verticalAlign:'middle',width:'100%' ,height:'auto'}} alt='banner4'  />
-          <Box sx={{
-            position: 'absolute',
-            top: '80px',
-            left:0,
-            right:0,
-            // transform:'translateY(50%)',
-            textAlign:'center'
-          }}>
-            <Typography variant='h5' sx={{ fontSize: 24, fontWeight: 400, color: 'white' }}>Nike Shoes</Typography>
-            <Typography variant='h2' sx={{ fontSize: 50, fontWeight: 700, color: 'white' }}>Collection 2019</Typography>
-          </Box>
+
+        <Grid container>
+          <Grid item lg={6} position='relative'>
+            <img src={banner3} alt='banner3' style={{ border: 'none', verticalAlign: 'middle', width: '100%', height: 'auto' }} />
+            <Box sx={{
+              position: 'absolute',
+              top: '50%',
+              transform: 'translateY(-50%)',
+              right: 0,
+              left: 0,
+              textAlign: 'center',
+              px: '30px'
+            }}>
+              <Typography variant='h5' sx={{ mb: 1, fontSize: 20, fontWeight: 400, color: '#222' }}>Hello Summer 2019</Typography>
+              <Typography variant='h2' sx={{ mb: 1, fontSize: 50, fontWeight: 700, color: '#222' }}>Extra 50%Off</Typography>
+              <Typography variant='subtitle1'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</Typography>
+              <Button sx={{ bgcolor: '#222', color: '#fff', mt: 2, fontSize: 18, textTransform: 'capitalize', fontWeight: 400, borderRadius: '50px', py: '10px', px: '50px' }}>More info</Button>
+            </Box>
+          </Grid>
+          <Grid item lg={6} position='relative'>
+            <img src={banner4} style={{ border: 'none', verticalAlign: 'middle', width: '100%', height: 'auto' }} alt='banner4' />
+            <Box sx={{
+              position: 'absolute',
+              top: '80px',
+              left: 0,
+              right: 0,
+              // transform:'translateY(50%)',
+              textAlign: 'center'
+            }}>
+              <Typography variant='h5' sx={{ fontSize: 20, fontWeight: 400, color: 'white' }}>Nike Shoes</Typography>
+              <Typography variant='h2' sx={{ fontSize: 50, fontWeight: 700, color: 'white' }}>Collection 2019</Typography>
+            </Box>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
     </Grid >
   )
 }
