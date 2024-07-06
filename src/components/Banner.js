1c7d456839ae8879237525752d19a5c6145859ca
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-07-06 19:11:53 +0500

    Code Refactoring

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Box,Typography, Button } from '@mui/material'
+import { Grid, Box, Typography, Button } from '@mui/material'
 import BannerImage from './BannerImage'
 import banner1 from './banner-1.webp'
 import banner2 from './banner-2.webp'
@@ -7,8 +7,51 @@ import banner3 from './banner-3.webp'
 import banner4 from './banner-4.webp'
 
 const Banner = () => {
+
+  const BannerHeading = ({ text, color = '#fff', font, marginBottom }) => {
+    console.log(font)
+    return <Typography variant='h2'
+      sx={{
+        fontSize: { ...font },
+        color,
+        fontWeight: 700,
+        mt: marginBottom && marginBottom,
+
+
+      }}>{text}</Typography>
+  }
+  const BannerSubHeading = ({ text, font, color = 'white', marginBottom }) => {
+
+    return <Typography variant='h5'
+      sx={{
+        fontSize: { ...font },
+        color,
+        fontWeight: 400,
+        mb: marginBottom && { ...marginBottom },
+      }}>
+      {text}
+    </Typography>
+  }
+  const BannerButton = ({ text, fontSize, marginTop }) => {
+
+    return (
+      <Button sx={{
+        fontSize,
+        color: '#fff',
+        fontWeight: 400,
+        bgcolor: '#222',
+        textTransform: 'capitalize',
+        borderRadius: '50px',
+        mt: marginTop,
+        px: '50px',
+        py: '10px',
+      }}>
+        {text}
+      </Button>
+    )
+  }
   return (
-    <Grid container px={{md:4,sm:2,xs:2}}>
+    <Grid container px={{ md: 4, sm: 2, xs: 2 }}>
       <Grid item lg={3.96} sm={3.96} xs={12} position='relative'>
         <BannerImage banner={banner1} />
         <Box sx={{
@@ -18,8 +61,8 @@ const Banner = () => {
           right: 0,
           textAlign: 'center'
         }}>
-          <Typography variant='h2' sx={{ fontSize: {md:50,sm:30,xs:40}, fontWeight: 700, color: 'white' }}>Run Don't Hide</Typography>
-          <Typography variant='h5' sx={{ fontSize: {md:20,sm:18,xs:18}, fontWeight: 400, color: 'white' }}>Nike Flash Be Seen</Typography>
+          <BannerHeading text="Run Don't Hide" font={{ md: 50, sm: 30, xs: 40 }} />
+          <BannerSubHeading text='Nike Flash Be Seen' font={{ md: 20, sm: 18, xs: 18 }} />
         </Box>
       </Grid>
 
@@ -31,12 +74,11 @@ const Banner = () => {
             top: '25%',
             right: '20px',
             transform: 'translateY(-50%)',
-
           }}>
-            <Typography variant='h5' sx={{ fontSize: {md:22,sm:20,xs:20}, fontWeight: 400, color: 'white' }}>Women's Training</Typography>
-            <Typography variant='h2' sx={{ fontSize: {md:50,sm:30,xs:30}, fontWeight: 700, color: 'white' }}>Don't Rest Bra</Typography>
+            <BannerSubHeading text="Women's Training" font={{ md: 22, sm: 20, xs: 20 }} />
+            <BannerHeading text="Don't Rest Bra" font={{ md: 50, sm: 30, xs: 30 }} />
             <Typography variant='body1' sx={{ fontSize: 24, fontWeight: 700, color: 'white', mb: 1, }}>Best Sellers</Typography>
-            <Button sx={{ bgcolor: '#222', color: '#fff', fontSize: 16, textTransform: 'capitalize', fontWeight: 400, borderRadius: '50px', py: '10px', px: '50px' }}>More info</Button>
+            <BannerButton text='More info' fontSize={16} />
           </Box>
         </Grid>
 
@@ -50,26 +92,27 @@ const Banner = () => {
               right: 0,
               left: 0,
               textAlign: 'center',
-              px: {md:'30px',sm:'20px'}
+              px: { md: '30px', sm: '20px' }
             }}>
-              <Typography variant='h5' sx={{ mb: {md:1,sm:0}, fontSize: {md:20,sm:16}, fontWeight: 400, color: '#222' }}>Hello Summer 2019</Typography>
-              <Typography variant='h2' sx={{ mb: 1, fontSize: {md:50,sm:30,xs:30}, fontWeight: 700, color: '#222' }}>Extra 50%Off</Typography>
-              <Typography variant='subtitle1' sx={{fontSize:{sm:12,xs:12}}}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</Typography>
-              <Button sx={{ bgcolor: '#222', color: '#fff', mt: 2, fontSize: 18, textTransform: 'capitalize', fontWeight: 400, borderRadius: '50px', py: '10px', px: '50px' }}>More info</Button>
+              <BannerSubHeading text="Hello Summer 2019" color='#222' font={{ md: 20, sm: 16 }} marginBottom={{ md: 1, sm: 0 }} />
+              <BannerHeading text="Extra 50%Off" marginBottom={1} color='#222' font={{ md: 50, sm: 30, xs: 30 }} />
+              <Typography variant='subtitle1' sx={{ fontSize: { sm: 12, xs: 12 } }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</Typography>
+              <BannerButton text='More info' fontSize={18} marginTop={2} />
             </Box>
           </Grid>
           <Grid item lg={6} sm={6} xs={6} position='relative'>
             <BannerImage banner={banner4} />
             <Box sx={{
               position: 'absolute',
-              top: {md:'80px',sm:'30px',xs:'30px'},
+              top: { md: '80px', sm: '30px', xs: '30px' },
               left: 0,
               right: 0,
-              // transform:'translateY(50%)',
+              // transform:'translateY(-50%)',
               textAlign: 'center'
             }}>
-              <Typography variant='h5' sx={{ fontSize: {md:20,sm:18,xs:18}, fontWeight: 400, color: 'white' }}>Nike Shoes</Typography>
-              <Typography variant='h2' sx={{ fontSize: {md:50,sm:30,xs:30}, fontWeight: 700, color: 'white' }}>Collection 2019</Typography>
+              <BannerSubHeading text="Nike Shoes" font={{ md: 20, sm: 16 }} />
+              <BannerHeading text="Collection 2019" marginBottom={1} font={{ md: 50, sm: 30, xs: 30 }} />
+
             </Box>
           </Grid>
         </Grid>
