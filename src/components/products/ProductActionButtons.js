272792ff2b9054da49551993ccee28e9e6ff3600
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-09-21 19:56:07 +0500

    Replace button with IconButton to make responsive

--- src/components/products/ProductActionButtons.js
+++ src/components/products/ProductActionButtons.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { ButtonGroup, Button } from '@mui/material';
+import { ButtonGroup, Button, IconButton } from '@mui/material';
 import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { useCartContext } from '../../context/cart';
 import { useAlertContext } from '../../context/alert';
 import { useModalContext } from '../../context/modal';
-
 
 const ProductActionButtons = ({ product, isHovered }) => {
 
@@ -30,6 +29,10 @@ const ProductActionButtons = ({ product, isHovered }) => {
         bgcolor: '#524938',
         color: 'white',
         border: 'none',
+        borderRadius: 1,
+
+        // p:'7px 16px',
+        // textAlign: 'center'
     }
     const hoverStyled = {
         transform: isHovered ? 'translateY(-50px)' : 'translateY(-10px)',
@@ -43,38 +46,39 @@ const ProductActionButtons = ({ product, isHovered }) => {
 
     }
     const quickViewButtonStyle = addDefaultStyle({
-        width: {md:'20%',sm:'33.333%',},
+        width: { md: '20%', sm: '33.333%', },
+        pb: 0.6,
         transition: 'opacity 300ms ease-in-out, transform 300ms ease-in-out, visibility 300ms ease-in-out',
         transitionDelay: '100ms',
-        display: {sm:'block',xs:'none'}
+        display: { sm: 'block', xs: 'none' }
     })
     const addToCartButtonStyle = addDefaultStyle({
-        width: {md:'60%',xs:'100%'},
+        width: { md: '60%', xs: '100%' },
+        fontSize: 11,
         borderLeft: '1px solid white',
         transition: 'opacity 400ms ease-in-out, transform 300ms ease-in-out, visibility 400ms ease-in-out',
         transitionDelay: '200ms',
-        display: {md:'block',sm:'none',xs:'block'}
-
+        display: { md: 'block', sm: 'none', xs: 'block' }
     })
     const addToCartIconStyle = addDefaultStyle({
         width: '33.333%',
+        pb: 0.2,
+        '&:hover': { bgcolor: 'red' },
         borderLeft: '1px solid white',
         transition: 'opacity 400ms ease-in-out, transform 300ms ease-in-out, visibility 400ms ease-in-out',
         transitionDelay: '200ms',
-        display:{md:'none',sm:'block',xs:'none'}
-
+        display: { md: 'none', sm: 'block', xs: 'none' }
     })
     const compareButtonStyle = addDefaultStyle({
-        width: {md:'21%',sm:'33.444%'},
+        width: { md: '21%', sm: '34.3333%' },
+        pb: 0.4,
         borderLeft: '1px solid white',
         transition: 'opacity 300ms ease-in-out, transform 300ms ease-in-out, visibility 600ms ease-in-out',
         transitionDelay: '300ms',
-        display: {sm:'block',xs:'none'}
-
+        display: { sm: 'block', xs: 'none' }
     })
 
     return (
-
         <ButtonGroup orientation="horizontal" size="large"
             sx={{
                 width: '100%',
@@ -85,13 +89,10 @@ const ProductActionButtons = ({ product, isHovered }) => {
                 right: 0,
             }}>
 
-            <Button onClick={handleViewModal} startIcon={<VisibilityIcon />} sx={quickViewButtonStyle} />
-
+            <IconButton aria-label="quickStyle button" onClick={handleViewModal} sx={quickViewButtonStyle}><VisibilityIcon /></IconButton>
             <Button onClick={handleCart} sx={addToCartButtonStyle}>ADD TO CART</Button>
-            <Button onClick={handleCart} sx={addToCartIconStyle} startIcon={<AddShoppingCartIcon />} />
-
-            <Button sx={compareButtonStyle} startIcon={<CompareArrowsIcon />} />
-
+            <IconButton aria-label="add to cart button" onClick={handleCart} sx={addToCartIconStyle}><AddShoppingCartIcon /></IconButton>
+            <IconButton aria-label="compare button" sx={compareButtonStyle}><CompareArrowsIcon /></IconButton>
         </ButtonGroup>
     )
 }
