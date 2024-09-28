c79d86ecd1fd8182fa5a04c48dff4ef62acb0724
sijadkhan1678 <sijadkhan1678@gmail.com>
2024-09-28 19:24:36 +0500

    Add cart operations to MiniCart

--- src/components/MiniCart.js
+++ src/components/MiniCart.js
@@ -3,10 +3,38 @@ import { Box, Stack, SwipeableDrawer, Divider, List, ListItem, ListItemText, Typ
 import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
 import RemoveCircleIcon from '@mui/icons-material/Remove'
-import data from '../context/data'
+import { useCartContext } from '../context/cart'
 import { Link } from 'react-router-dom'
 
 const CartItem = ({ cartItem }) => {
+
+    const { calculateSubTotal, calculateTotal, increaseQuantity, decreaseQuantity, removeFromCart } = useCartContext()
+
+    const handleCartOperation = (operationType) => {
+        switch (operationType) {
+
+            case 'REMOVE_FROM_CART':
+                removeFromCart(cartItem.id)
+                calculateSubTotal()
+                calculateTotal()
+                break;
+
+            case 'INCREASE_QTY':
+                increaseQuantity(cartItem.id)
+                calculateSubTotal()
+                calculateTotal()
+                break;
+
+            case 'DECREASE_QTY':
+                decreaseQuantity({ id: cartItem.id, quantity: cartItem.quantity })
+                calculateSubTotal()
+                calculateTotal()
+                break
+
+            default:
+                return
+        }
+    }
     return (
         <Stack direction="row" gap={2} mb={2} >
 
@@ -16,29 +44,29 @@ const CartItem = ({ cartItem }) => {
                 objectFit: 'cover',
             }} />
 
-            <Stack width="100%" sx={{position:'relative'}}>
+            <Stack width="100%" sx={{ position: 'relative' }}>
                 <Stack direction="row" width='100%' justifyContent="space-between" alignItems="center" mb={3}>
                     <Typography components='h4' sx={{ fontSize: { sm: 15, xs: 16, }, fontWeight: 700, color: "#1c1c1c", }}>
                         {cartItem.name}
                     </Typography>
 
-                    <IconButton sx={{ color: 'gray',m: 0, p: 0 }}>
-                        <DeleteIcon sx={{fontSize:18}}/>
+                    <IconButton onClick={() => handleCartOperation("REMOVE_FROM_CART")} sx={{ color: 'gray', m: 0, p: 0 }}>
+                        <DeleteIcon sx={{ fontSize: 18 }} />
                     </IconButton>
                     {/* <DeleteIcon disabled style={{ color: '#d32f2f', }} /> */}
                 </Stack>
-                <Stack direction='row'  sx={{justifyContent:"space-between",alignItems:"center",position:"absolute",bottom:0,left:0,right:0}}>
+                <Stack direction='row' sx={{ justifyContent: "space-between", alignItems: "center", position: "absolute", bottom: 0, left: 0, right: 0 }}>
                     <Typography variant='h6' color='#1c1c1c' sx={{ fontSize: 14, fontWeight: 700, color: "#1c1c1c", }}>
                         ${cartItem.price}
                     </Typography>
-                    <Stack direction='row'  sx={{justifyContent:'space-between', alignItems:'center',border:'1px solid whitesmoke'}} >
+                    <Stack direction='row' sx={{ justifyContent: 'space-between', alignItems: 'center', border: '1px solid whitesmoke' }} >
 
 
-                        <IconButton sx={{color:'#1c1c1c', }}><AddIcon sx={{fontSize:16}}/></IconButton>
+                        <IconButton onClick={() => handleCartOperation('INCREASE_QTY')} sx={{ color: '#1c1c1c', }}><AddIcon sx={{ fontSize: 16 }} /></IconButton>
 
-                        <Typography variant='body2' px={1}>2</Typography>
+                        <Typography variant='body2' px={1}>{cartItem.quantity}</Typography>
 
-                        <IconButton sx={{color:'#1c1c1c' }}><RemoveCircleIcon sx={{fontSize:16}}/></IconButton>
+                        <IconButton onClick={() => handleCartOperation('DECREASE_QTY')} sx={{ color: '#1c1c1c' }}><RemoveCircleIcon sx={{ fontSize: 16 }} /></IconButton>
 
 
                     </Stack>
@@ -49,12 +77,14 @@ const CartItem = ({ cartItem }) => {
     )
 }
 const MiniCart = () => {
+    const { item, subTotalPrice } = useCartContext()
+    console.log('minicart items', item)
     const [open, setOpen] = useState(true)
+
     const toggleCartDrawer = () => {
         // setOpen(!open)
     }
 
-    const cartItems = data.slice(0,20)
     return (
         <Fragment>
             <SwipeableDrawer open={open} anchor="right" onOpen={toggleCartDrawer} onClose={toggleCartDrawer} sx={{ '.MuiPaper-root::-webkit-scrollbar': { width: '0px' } }}>
@@ -62,25 +92,20 @@ const MiniCart = () => {
                 <Box
                     sx={{ width: 400, }}
                     role="presentation"
-                    onClick={toggleCartDrawer}
-                >
+                    onClick={toggleCartDrawer}>
                     <Stack sx={{ px: 3, py: 3, position: 'relative', }}>
                         <Box pb={3}>
                             <Typography components="h3" sx={{
                                 fontSize: 22,
                                 fontWeight: 700,
-
                                 // textAlign:'center'
                             }}>Shopping Cart</Typography>
                         </Box>
                         <Divider />
 
-
                         <Box mt={3}>
 
-                            {cartItems.map((item) => <CartItem cartItem={item} />)}
-
-
+                            {item.map((item) => <CartItem key={item.id} cartItem={item} />)}
 
                         </Box>
                     </Stack>
@@ -101,18 +126,18 @@ const MiniCart = () => {
                                 fontWeight: 700
                             }}>
                                 Subtotal
-                                </Typography>
+                            </Typography>
 
                             <Typography component="h3" sx={{
                                 fontSize: 20,
                                 fontWeight: 700
                             }}>
-                                {/* ${subTotalPrice.toFixed(2)} */} $200
-                                </Typography>
+                                ${subTotalPrice.toFixed(2)}
+                            </Typography>
 
                         </Stack>
                         <Box px={3}>
-                        <Button fullWidth component={Link} to='/cart' variant="contained" size="large"
+                            <Button fullWidth component={Link} to='/cart' variant="contained" size="large"
                                 sx={{
                                     py: 1.5,
                                     mb: 2,
@@ -123,14 +148,12 @@ const MiniCart = () => {
                             > View Cart </Button>
                             <Button fullWidth component={Link} to='/cart/checkout' variant="contained" size="large"
                                 sx={{
-                                    
                                     py: 1.5,
                                     fontSize: 14,
-
                                     bgcolor: "#524938",
                                     '&:hover': { bgcolor: '#ff7800' }
                                 }}
-                            >Continue Checkout </Button>   
+                            >Continue Checkout </Button>
                         </Box>
                     </Box>
 
